3165259a2361fc8a51bbd5f04c57a025eeee7e1b
Rasbats <miker@members.swis.net>
2022-03-09 11:57:13 +0000

    try another path for the temp file

--- src/CanadianTidesgui_impl.cpp
+++ src/CanadianTidesgui_impl.cpp
@@ -408,8 +408,9 @@ void Dlg::OnDownload(wxCommandEvent& event) {
 	wxString urlString = "https://api-iwls.dfo-mpo.gc.ca/api/v1/stations?chs-region-code=" + choiceRegion + "&&time-series-code=wlp-hilo";
 	wxURI url(urlString);
 
-	wxString stdpath = StandardPath();
-	wxString tmp_file = wxFileName::CreateTempFileName(stdpath);
+	wxString tmp_path = StandardPath();
+	wxString s = wxFileName::GetPathSeparator();
+	wxString tmp_file = tmp_path + s + "temp.txt";
 	wxMessageBox(tmp_file);
 
 	_OCPN_DLStatus ret = OCPN_downloadFile(url.BuildURI(), tmp_file,
@@ -653,8 +654,9 @@ void Dlg::getHWLW(string id)
 	wxString urlString = "https://api-iwls.dfo-mpo.gc.ca/api/v1/stations/" + id + tidalevents + code + fromDate + snow + toDate + snowplus;
 	wxURI url(urlString);
 
-	wxString stdpath = StandardPath();
-	wxString tmp_file = wxFileName::CreateTempFileName(stdpath);
+	wxString tmp_path = StandardPath();
+	wxString s = wxFileName::GetPathSeparator();
+	wxString tmp_file = tmp_path + s + "temp.txt";
 	wxMessageBox(tmp_file);
 
 	_OCPN_DLStatus ret = OCPN_downloadFile(url.BuildURI(), tmp_file,
