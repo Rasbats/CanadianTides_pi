6e75355816916703df32e685b98523a4e6ce9b9c
Rasbats <miker@members.swis.net>
2025-09-16 12:26:26 +0100

    v0.9.2

--- Plugin.cmake
+++ Plugin.cmake
@@ -31,7 +31,7 @@ set(OCPN_RELEASE_REPO
 # -------  Plugin setup --------
 #
 set(PKG_NAME CanadianTides_pi)
-set(PKG_VERSION  0.9.0)
+set(PKG_VERSION  0.9.2)
 set(PKG_PRERELEASE "")  # Empty, or a tag like 'beta'
 
 set(DISPLAY_NAME CanadianTides)    # Dialogs, installer artifacts, ...
